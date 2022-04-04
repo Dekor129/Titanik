230d021dd7eb4cda7d7754fd4b6fd67232264802
Dekor129 <95143910+Dekor129@users.noreply.github.com>
2022-04-04 20:48:15 +0300

    Update passengers.js

--- passengers.js
+++ passengers.js
@@ -56,7 +56,7 @@ function inTable (response, table, i ) {
     (response[i].survived == true) ? table.rows[0].cells[0].innerHTML = 'Survived' : table.rows[0].cells[0].innerHTML = 'Not Survived';
     table.rows[0].cells[1].innerHTML = '№ ' + response[i].id;
     (response[i].name == false || response[i].name == null) ? table.rows[1].cells[0].innerHTML = 'Name: Unknown' : table.rows[1].cells[0].innerHTML = 'Name: ' + response[i].name;
-    (response[i].age == false || response[i].age == null) ? table.rows[2].cells[0].innerHTML = 'Age: Unknown' : table.rows[2].cells[0].innerHTML = 'Age: ' + Math.floor(response[i].age);
+    (response[i].age == false || response[i].age == null) ? table.rows[2].cells[0].innerHTML = 'Full age: Unknown' : table.rows[2].cells[0].innerHTML = 'Full age: ' + Math.floor(response[i].age);
     (response[i]['home.dest'] == false || response[i]['home.dest'] == null) ? table.rows[3].cells[0].innerHTML = 'Home: Unknown' : table.rows[3].cells[0].innerHTML = 'Home: ' + response[i]['home.dest'];
     (response[i].gender == false || response[i].gender == null) ? table.rows[4].cells[0].innerHTML = 'Gender: Unknown': table.rows[4].cells[0].innerHTML = 'Gender: ' + response[i].gender;                
     (response[i].ticket == false || response[i].ticket == null) ? table.rows[5].cells[0].innerHTML = 'Ticket: Unknown' : table.rows[5].cells[0].innerHTML = 'Ticket: ' + response[i].ticket;
